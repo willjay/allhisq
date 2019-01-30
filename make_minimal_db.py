9865ebe82d2f30e3627137984f5bc8f239a00d5e
William Jay <wjay@lattice.fnal.gov>
2019-01-30 13:43:33 -0600

    Corrected minor typos 	modified:   db_connection.py 	modified:   make_minimal_db.py

--- make_minimal_db.py
+++ make_minimal_db.py
@@ -7,75 +7,83 @@ def main():
 
     ensemble = """
     CREATE TABLE IF NOT EXISTS ensemble
-    id serial PRIMARY KEY,
-    name text,  -- e.g., "HISQ 2+1+1"
-    ns integer, 
-    nt integer,
-    UNIQUE(name, ns, nt);
-    """
+    (
+      id serial PRIMARY KEY,
+      name text,  -- e.g., "HISQ 2+1+1"
+      ns integer, 
+      nt integer,
+      UNIQUE(name, ns, nt)
+    );"""
 
     correlation_function = """
     CREATE TABLE IF NOT EXISTS correlation_function
-    id SERIAL PRIMARY KEY,
-    ens_id integer REFERENCES ensemble (id),
-    name   text NOT NULL DEFAULT '',
-    UNIQUE(ens_id, name);
-    """
+    (
+      id SERIAL PRIMARY KEY,
+      ens_id integer REFERENCES ensemble (id),
+      name   text NOT NULL DEFAULT '',
+      UNIQUE(ens_id, name)
+    );"""
 
     reduction_two_point = """
     CREATE TABLE IF NOT EXISTS reduction_two_point
-    id SERIAL PRIMARY KEY,
-    ens_id integer REFERENCES ensemble (id),
-    fold           boolean NOT NULL, 
-    block_length   integer NOT NULL,
-    nsamples       integer NOT NULL,
-    n_time_sources integer NOT NULL,
-    log            text DEFAULT '',
-    UNIQUE(fold, block_length, nsamples, n_time_sources);
-    """
+    (
+      id SERIAL PRIMARY KEY,
+      ens_id integer REFERENCES ensemble (id),
+      fold           boolean NOT NULL, 
+      block_length   integer NOT NULL,
+      nsamples       integer NOT NULL,
+      n_time_sources integer NOT NULL,
+      log            text DEFAULT '',
+      UNIQUE(fold, block_length, nsamples, n_time_sources)
+    );"""
 
     analysis_two_point = """
     CREATE TABLE IF NOT EXISTS analysis_two_point
-    id SERIAL PRIMARY KEY,
-    tmin        integer,         -- Inclusive
-    tmax        integer,         -- Exclusive
-    prior_alias text DEFAULT '', -- Some quick nickname
-    prior       text DEFAULT '', -- The prior itself
-    UNIQUE(tmin, tmax, prior_alias);
-    """
+    (
+      id SERIAL PRIMARY KEY,
+      tmin        integer,         -- Inclusive
+      tmax        integer,         -- Exclusive
+      prior_alias text DEFAULT '', -- Some quick nickname
+      prior       text DEFAULT '', -- The prior itself
+      UNIQUE(tmin, tmax, prior_alias)
+    );"""
 
     results_two_point = """
     CREATE TABLE IF NOT EXISTS result_two_point
-    id SERIAL PRIMARY KEY
-    ens_id       integer REFERENCES correlation_function (ens_id),
-    corr_id      integer REFERENCES correlation_function (id),
-    reduction_id integer REFERENCES reduction_two_point (id),
-    anal_id      integer REFERENCES analysis_two_point (id),
-    chi2    float,
-    npoints integer,
-    q       float,
-    params  text,
-    log     text DEFAULT '',
-    calcdate timestamp with time zone,
-    UNIQUE(ens_id, corr_id, reduction_id, anal_id);
-    """
+    ( 
+      id SERIAL PRIMARY KEY,
+      ens_id       integer REFERENCES ensemble (id),
+      corr_id      integer REFERENCES correlation_function (id),
+      reduction_id integer REFERENCES reduction_two_point (id),
+      anal_id      integer REFERENCES analysis_two_point (id),
+      chi2    float,
+      npoints integer,
+      q       float,
+      params  text,
+      log     text DEFAULT '',
+      calcdate timestamp with time zone,
+      UNIQUE(ens_id, corr_id, reduction_id, anal_id)
+    );"""
 
     campaign_two_point = """
     CREATE TABLE IF NOT EXISTS campaign_two_point
-    id SERIAL PRIMARY KEY, 
-    result_id integer REFERENCES result_two_point (id)
-    ens_id    integer REFERENCES result_two_point (ens_id), 
-    corr_id   integer REFERENCES result_two_point (corr_id),
-    UNIQUE(ens_id, corr_id);
-    """
+    (
+      id SERIAL PRIMARY KEY, 
+      result_id integer REFERENCES result_two_point (id),
+      ens_id    integer REFERENCES ensemble (id), 
+      corr_id   integer REFERENCES correlation_function (id),
+      UNIQUE(ens_id, corr_id)
+    );"""
 
     systematic_two_point = """
-    id SERIAL PRIMARY KEY,
-    ens_id  integer REFERENCES campaign_two_point (ens_id)
-    corr_id integer REFERENCES campatin_two_point (corr_id)
-    systematic text NOT NULL;
-    UNIQUE(ens_id, corr_id);
-    """
+    CREATE TABLE IF NOT EXISTS systematic_two_point
+    (
+      id SERIAL PRIMARY KEY,
+      ens_id  integer REFERENCES ensemble (id),
+      corr_id integer REFERENCES correlation_function (id),
+      systematic text NOT NULL,
+      UNIQUE(ens_id, corr_id)
+    );"""
 
     engine = db.make_engine()
 
@@ -89,6 +97,7 @@ def main():
         systematic_two_point
     ]
     for query in queries:
+        print(query)
         engine.execute(query)
 
 
