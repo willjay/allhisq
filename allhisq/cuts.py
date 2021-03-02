b80bb721e865193bd1d78c95233f48e129d4b77b
William Jay <wjay@fnal.gov>
2021-03-01 21:10:03 -0600

    Move check_tsm_source to hdf5_cache

--- allhisq/cuts.py
+++ allhisq/cuts.py
@@ -43,7 +43,7 @@ def get_data_with_tsm_status(engine, basename):
     # Grab pre-TSM raw data from database
     pre_tsm = hdf5.get_pre_tsm_raw_data(basename, engine)
     # Infer status of TSM sources (even, odd, mixed)
-    status = check_tsm_sources(pre_tsm).sort_values(by=cols)
+    status = hdf5.check_tsm_sources(pre_tsm).sort_values(by=cols)
     # Grab post-TSM raw data from cache
     post_tsm = hdf5.get_correlator(engine, basename)
     # Align data with status codes
@@ -51,77 +51,3 @@ def get_data_with_tsm_status(engine, basename):
     data['data'] = [config for config in post_tsm]
     data = pd.merge(data, status, on=cols)
     return data
-
-
-def check_tsm_sources(pre_tsm):
-    """
-    Checks the sources used for the truncated solver method (TSM), which are
-    either even or odd.
-    Full (fine+loose) solves:
-        * even: all sources, both fine and loose, are even
-        * odd: all sources, both fine and loose, are odd
-        * mixed: a matching even fine-loose pair exists; all of the remaining
-          loose solves are odd
-        * missing match: no matching fine-loose pair exists
-        * unknown: any case not in 'even', 'odd', 'mixed', or 'missing match'
-    Fine solves / loose solves:
-        * even: all fine/loose solves use even sources
-        * odd: all fine/loose solves use odd sources
-        * mixed: the fine/loose solves use a mixture of even and odd sources
-    Args:
-        pre_tsm: DataFrame <more description...>
-    Returns:
-        count: DataFrame <more description...>
-    """
-    counts = []
-    groups = pre_tsm.groupby(['series', 'trajectory'])
-    for (series, trajectory), group in groups:
-        fine = (group['solve_type'] == 'fine')
-        loose = ~fine
-        # match = (group[loose]['tsrc'] == group[fine]['tsrc'].item())
-        match = group[loose]['tsrc'].isin(group[fine]['tsrc'])
-        n_match = bool(len(group[loose][match]))
-
-        # Full (fine + loose) solves
-        n_odd = np.sum(group['tsrc'].values % 2)
-        n_even = len(group) - n_odd
-        if n_odd == 0:
-            status_full = 'even'
-        elif n_odd == len(group):
-            status_full = 'odd'
-        elif n_match > 0:
-            if n_even == 2:
-                status_full = 'mixed'
-            else:
-                status_full = 'unknown'
-        else:
-            status_full = 'missing match'
-
-        # Fine solves only
-        n_odd = np.sum(group[fine]['tsrc'].values % 2)
-        if n_odd == 0:
-            status_fine = 'even'
-        elif n_odd == len(group[fine]):
-            status_fine = 'odd'
-        else:
-            status_fine = 'mixed'
-
-        # Loose solves only
-        n_odd = np.sum(group[loose]['tsrc'].values % 2)
-        if n_odd == 0:
-            status_loose = 'even'
-        elif n_odd == len(group[loose]):
-            status_loose = 'odd'
-        else:
-            status_loose = 'mixed'
-
-        counts.append({
-            'series': series,
-            'trajectory': trajectory,
-            'status:full': status_full,
-            'status:fine': status_fine,
-            'status:loose': status_loose,
-        })
-
-    return pd.DataFrame(counts)
- 
