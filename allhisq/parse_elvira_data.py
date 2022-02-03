0b4712496ff9c6f0b17d3393c80f9e26999203d3
William Jay <wjay@fnal.gov>
2022-02-03 15:52:22 -0600

    Tidy comments and remove code fragments

--- allhisq/parse_elvira_data.py
+++ allhisq/parse_elvira_data.py
@@ -1,40 +1,8 @@
 """
+Parses Elvira's 2pt and 3pt data at q2=0 into the database and automation pipeline.
 Ref: https://arxiv.org/pdf/1901.08989.pdf
-
 Bare quark masses for input from
 Ref: https://arxiv.org/pdf/1411.1651.pdf
-
-
-Need entries in tables:
-DONE: ensemble
-DONE: lattice_spacing
-strong_coupling --> can probably just copy the table with appropriate tweaks
-DONE: form_factor
-DONE: alias_form_factor
-DONE: sign_form_factor
-campaign_results_two_point --> Must be done at the level of fits
-    Hmm. Actually we need the ids:
-        * result_id serial PRIMARY KEY,
-        * ens_id integer REFERENCES ensemble(ens_id),
-        * corr_id integer REFERENCES correlator_n_point(corr_id),
-    --> at the very least, we'll need to write entires for correlator_n_point
-    Fortunately, these should be easy, since all we need to supply are
-        * ens_id
-        * name
-
-
-
-
-
-
-for db_io.get_form_factor_data, we need entries in tables:
-junction_form_factor
-correlator_n_point
-Hmm. Since this function also aliases the correlators, it might make sense
-simply to write a new function. Perhaps something like 'get_form_factor_data_elvira'
-which would return a data dictionary to match the original function.
-This would mean that...??
-
 """
 import pandas as pd
 import numpy as np
@@ -281,7 +249,7 @@ def main():
             }
             sql.queries.write_external_database(conn, **record)
 
-
+# ----- end main ----- #
 
 
 def h5write(h5fname, basename, data):
@@ -297,27 +265,9 @@ def h5write(h5fname, basename, data):
             dset.attrs['basename'] = basename
             dset.attrs['calcdate'] = datetime.datetime.now().isoformat()
         except ValueError:
+            # This entry (probably) already exists.
             pass
 
-    # corr2 = pd.DataFrame(corrs[corrs['corr_type'].isin(('light-light','heavy-light'))])
-    # corr3 = pd.DataFrame(corrs[corrs['corr_type']=='three-point'])
-
-    # print(corr2)
-
-    # corr2['state'] = corrs['corr_name'].apply(identify_state)
-
-    # print(corr2['state'].unique())
-
-    # form_factor_id integer REFERENCES form_factor(form_factor_id),
-    # corr_id integer REFERENCES correlator_n_point(corr_id),
-    # corr_type text NOT NULL,
-    # UNIQUE(form_factor_id, corr_id, corr_type)
-
-
-
-
-
-# ----- end main ----- #
 
 def make_engine(settings, echo=False):
     url = 'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
@@ -336,49 +286,6 @@ class SQLWrapper:
         return connection_scope(self.engine)
 
 
-
-# 2ptDmom0
-# 2ptpimom2
-# 2ptpimom1
-# 2ptpimom0
-# 2ptKmom0
-# 2ptKmom1
-# 2ptKmom2
-
-# 3ptDKT15
-# 3ptDKT18
-# 3ptDKT20
-# 3ptDKT21
-# 3ptDKT22
-
-# 3ptDPT15
-# 3ptDPT18
-# 3ptDPT20
-# 3ptDPT21
-# 3ptDPT22
-
-# 3ptDPm0T15
-# 3ptDPm0T18
-# 3ptDPm0T20
-# 3ptDPm0T21
-# 3ptDPm0T22
-
-# 3ptDKm0T15
-# 3ptDKm0T18
-# 3ptDKm0T20
-# 3ptDKm0T21
-# 3ptDKm0T22
-
-# Infer 3pt functions
-# regex = re.compile(r"^3ptDPT(\d\d?)$")
-# for key in data:
-#     match = regex.match(key)
-#     if match:
-#         T, = match.groups()
-#         key_map[key] = int(T)
-
-# tmp = {}
-
 def infer_corr_type(key):
     valid_names = {
         'light-light': re.compile(r"^2pt(pi|K)mom(\d)$"),
@@ -390,6 +297,7 @@ def infer_corr_type(key):
             return name
     raise ValueError("Unrecognized correlator name", key)
 
+
 def identify_state(key):
     regex = re.compile(r"^2pt(pi|K|D|Ds)mom(\d)$")
     match = regex.match(key)
@@ -397,6 +305,7 @@ def identify_state(key):
         raise ValueError("Unable to identify state from", key)
     state = match.groups()[0]
     return state
+
 
 def identify_transition(key):
     regex = re.compile(r"^3pt(D|K)(P|K)((m0)?)T(\d\d?)$")
@@ -418,96 +327,6 @@ def identify_transition(key):
         'daughter': daughter,
         'q2=0': keep,
     }
-
-
-
-    # if light_light.match(key):
-    #     return light_light
-
-
-
-    # pion_2pt = re.compile(r"^2ptpimom(\d)$")
-    # kaon_2pt = re.compile(r"^2ptKmom(\d)$")
-    # d_2pt = re.compile(r"^2ptDmom(\d)$")
-
-    # if pion_2pt.match(key) or kaon_2pt.match(key):
-    #     return 'light-light'
-
-    # if d_2pt.match(key):
-    #     return 'heavy_light'
-
-    # # 3pt functions
-    # d2pi_3pt_q2zero = re.compile(r"^3ptD(P|K)(m0?)T(\d\d?)$")
-
-    # d2pi_3pt_q2zero = re.compile(r"^3ptDPT(\d\d?)$")
-    # d2k_3pt_q2zero = re.compile(r"^3ptDKT(\d\d?)$")
-    # regex = re.compile(r"^3ptDKm0T(\d\d?)$")
-    # regex = re.compile(r"^3ptDPm0T(\d\d?)$")
-
-    # if
-
-    # , d_2pt]:
-    #     if regex.match(key):
-    #         return
-    #     if match:
-
-
-
-
-    # 3pt functions for d2pi and d2k at q2=0
-
-    # # D2pi 3pt functions, q2=q2max
-    # regex = re.compile(r"^3ptDKm0T(\d\d?)$")
-
-    # # D2K 3pt functions, q2=q2max
-    # regex = re.compile(r"^3ptDPm0T(\d\d?)$")
-
-
-def rename(key, masses):
-    """
-    2pt functions have names like
-    A4-A4_RW_RW_d_d_m0.389_m0.0363_p000-fine
-    P5-P5_RW_RW_d_d_m0.0012_m0.0012_p000-fine
-    Note that
-
-    For the pion and kaon 2pt functions, the following tags are used
-    * mom0 = pion at rest
-    * mom1 = pion with momentum such that q2=0 for K2pi
-    * mom2 = pion with momentum such that q2=0 for D2pi
-
-
-    """
-    # template_2pt = "P5-P5_RW_RW_d_d_m{mass1}_mass{2}_q2=0"
-
-
-    # 2pt functions
-    pion_2pt = re.compile(r"^2ptpimom(\d)$")
-    kaon_2pt = re.compile(r"^2ptKmom(\d)$")
-    d_2pt = re.compile(r"^2ptDmom(\d)$")
-
-
-    # 3pt functions for d2pi and d2k at q2=0
-    d2pi_3pt = re.compile(r"^3ptDPT(\d\d?)$")
-    d2k_3pt = re.compile(r"^3ptDKT(\d\d?)$")
-
-
-
-
-
-    # OK to skip data at q2=q2max?
-
-    # # kaon 2pt functions
-    # # mom0 = kaon at rest
-    # # mom1 = kaon with momentum such that q2=0 for ???
-    # # mom2 = kaon with momentum such that q2=0 for D2K
-    # kaon_2pt = re.compile(r"^2ptKmom(\d)$")
-
-
-    # # D2pi 3pt functions, q2=q2max
-    # regex = re.compile(r"^3ptDKm0T(\d\d?)$")
-
-    # # D2K 3pt functions, q2=q2max
-    # regex = re.compile(r"^3ptDPm0T(\d\d?)$")
 
 
 def read_data(fname):
@@ -533,8 +352,6 @@ def read_data(fname):
     for key in data:
         data[key] = np.array(data[key])
     return data
-
-
 
 
 if __name__ == '__main__':
